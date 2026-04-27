3ca31e9dcab07d4d7b748026ffd362b5d92af051
ShahdEid2003 <shahdeid012@gmail.com>
2026-04-28 00:57:27 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,26 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
+  preview: {
+    allowedHosts: [
+      "palestine-educational-academy-summer-camp.onrender.com",
+    ],
+  },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+    dedupe: [
+      "react",
+      "react-dom",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+      "@tanstack/react-query",
+      "@tanstack/query-core",
+    ],
   },
 }));
